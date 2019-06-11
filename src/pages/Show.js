c18dffdcfbd78a0628a18c705f92255d5bfe372d
maxiaobu1999 <maqinglong1999@163.com>
2019-06-11 22:02:14 +0800

    【组件】导入自定义组件

---
+++ src/pages/Show.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import HelloWorld from "./index";
+class Show  extends React.Component{
+    render() {
+        return (
+            <HelloWorld></HelloWorld>
+        );
+    }
+}
+export default Show;
